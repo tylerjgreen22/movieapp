7c423f73828048aa8a8e6f4260623f3a5c224596
Tyler <tylerjgreen22@gmail.com>
2022-09-21 00:27:34 -0700

    Added CSS

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
-import React from "react";
+import { React, useEffect } from "react";
+
+//api key
+//e3a7437e
+
+const API_URL = 'http://www.omdbapi.com?apikey=e3a7437e';
 
 const App = () => {
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovies('Spiderman')
+    }, []);
+
     return (
         <h1>App</h1>
     );
