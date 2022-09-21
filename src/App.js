d766f95c7d43e420be5e269bb47e208c94446f2d
Tyler <tylerjgreen22@gmail.com>
2022-09-21 01:02:40 -0700

    Updated App and MovieCard

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { React, useEffect } from "react";
+import { React, useEffect, useState } from "react";
 import MovieCard from "./MovieCard";
 
 import './App.css';
@@ -6,26 +6,28 @@ import SearchIcon from './search.svg';
 
 const API_URL = 'http://www.omdbapi.com?apikey=e3a7437e';
 
-const movie1 = {
-    "Title": "Amazing Spiderman Syndrome",
-    "Year": "2012",
-    "imdbID": "tt2586634",
-    "Type": "movie",
-    "Poster": "N/A"
-}
-
 const App = () => {
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState([]);
 
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
         const data = await response.json();
 
-        console.log(data.Search);
+        setMovies(data.Search);
     }
 
     useEffect(() => {
-        searchMovies('Spiderman')
+        searchMovies('')
     }, []);
+
+    const handleChange = (e) => {
+        setSearchTerm(e.target.value);
+    }
+
+    const handleClick = () => {
+        searchMovies(searchTerm);
+    }
 
     return (
         <div className="app">
@@ -34,18 +36,28 @@ const App = () => {
             <div className="search">
                 <input
                     placeholder="Search for movies"
-                    value="Superman"
-                    onChange={() => { }}
+                    value={searchTerm}
+                    onChange={handleChange}
                 />
                 <img
                     src={SearchIcon}
                     alt="search"
-                    onClick={() => { }}
+                    onClick={handleClick}
                 />
             </div>
-            <div className="container">
-                <MovieCard movie1={movie1}/>
-            </div>
+
+            {movies?.length > 0 ? (
+                <div className="container">
+                    {movies.map((movie) => (
+                        <MovieCard movie={movie} />
+                    ))}
+                </div>
+            ) : (
+                <div className="empty">
+                    <h2>Search for a movie</h2>
+                </div>
+            )}
+
         </div>
     );
 }
